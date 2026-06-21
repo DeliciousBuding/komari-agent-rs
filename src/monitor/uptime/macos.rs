cfe6f068cb8ca45a64a5436e8ee5b9af63105c86
Delicious233 <101502465+DeliciousBuding@users.noreply.github.com>
2026-06-21 22:41:34 +0800

    fix(macos): cast c-str as_ptr to *const u8 for sysctlbyname

--- src/monitor/uptime/macos.rs
+++ src/monitor/uptime/macos.rs
@@ -42,7 +42,7 @@ pub fn collect_uptime() -> Result<u64, io::Error> {
 
     let ret = unsafe {
         sysctlbyname(
-            c"kern.boottime".as_ptr(),
+            c"kern.boottime".as_ptr() as *const u8,
             (&mut tv) as *mut TimeVal as *mut u8,
             &mut len,
             std::ptr::null(),
